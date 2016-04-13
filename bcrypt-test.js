1071c8725c4a9ed58470fea138093f1f1c999b43
Naphan Benchasattabuse <Naphan Benchasattabuse>
2016-04-14 01:06:18 +0700

    fix typos

--- bcrypt-test.js
+++ bcrypt-test.js
@@ -1,6 +1,6 @@
 var bcrypt = require('bcrypt-nodejs');
 
-bcrypt.compare('poki', '$2a$10$R1inoiZrO/g6B3IbwmY7ROK7UBN34sO4n/b4xg3dWYt2Vfcsg.jvm', function(err, results) {
+bcrypt.compare('paul', '$2a$10$FlqBp1ZEmDJVZb4gkYd9ruzR7DohcRxkBPcFuq9MMtuf17swDCYSC', function(err, results) {
     if (err) {
         console.error(err);
     } else {
