7d93fe52708bbb4b33b2deafb16430aa9fc3fe4a
Naphan Benchasattabuse <osiris_thedead@hotmail.com>
2016-04-25 13:06:10 +0700

    add click methods

--- client/static/js/all-room.js
+++ client/static/js/all-room.js
@@ -1,63 +1,86 @@
 var allRoomData = {
-            items: [
-                { name: 'SA Room'  },
-                { name: 'Dis Sys Room' },
-                { name: 'TechWrite Room' },
-                { name: 'CP40 Room' },
-                { name: 'CP40.5 Room' },
-                { name: 'SensorTech Room'  },
-                { name: 'Teacup Room'  }
-            ],
-            newRoomInput: ''
-        };
+    items: [
+        {
+            name: 'SA Room',
+            roomId: 1
+        },
+        {
+            name: 'Dis Sys Room',
+            roomId: 2
+        },
+        {
+            name: 'TechWrite Room',
+            roomId: 3
+        },
+        {
+            name: 'CP40 Room',
+            roomId: 4
+        },
+        {
+            name: 'CP40.5 Room',
+            roomId: 5
+        },
+        {
+            name: 'SensorTech Room',
+            roomId: 6
+        },
+        {
+            name: 'Teacup Room',
+            roomId: 7
+        }
+    ],
+    newRoomInput: ''
+};
 
 var allRoom = Vue.extend({
-    template:   '<<div id="my-nav">\
-                <my-navbar></my-navbar>\
-            </div>\
-            <div class="page-header" style="text-align: center;">\
-                <h1 id="page-tite"> \
-                <span  class="all-room-label label label-me"> \
-                    <a href="./all-room.html"> All Room </a>\
-                </span>\
-                </h1>\
-            </div>\
-            <div class="new-room-box" id="add-room-box">\
-                <div class="row well well-lg form-group">\
-                    <div class="col-sm-1"><span class="glyphicon glyphicon-plus"></span></div>\
-                    <div class="col-sm-9">\
-                        <input type="text" class="form-control new-room-name" \
-                        placeholder=" room name" v-model="newRoomInput" v-on:keyup.enter="addRoom"> \
-                    </div>\
-                    <div class="col-sm-2">\
-                        <button class="btn btn-success" v-on:click="addRoom">\
-                            <span class="new-btn">Create</span>   \
-                        </button>\
-                    </div>\
-                </div>\
-            </div> \
-            <div class="room-list" id="all-list">\
-                <div class="row well well-lg" v-for="item in items">\
-                    <div class="col-sm-1"><span class="glyphicon glyphicon-star"></span></div>\
-                    <div class="col-sm-9"><a href="#">{{ item.name }}</a></div>\
-                    <div class="col-sm-2">\
-                        <button class="btn btn-warning">\
-                            <span class="join-btn">Join</span>   \
-                        </button>\
-                    </div>\
-                </div>\
-            </div>'
-       ,
-       data : function  () {
-           return allRoomData;
-       } ,
-       methods: {
-            addRoom: function () {
-                var name = this.newRoomInput.trim()
-                if (name) {
-                    allRoomData.items.unshift({ name: name })
-                    this.newRoomInput = ''
-                }
+    template: '<div id="my-nav">\
+                   <my-navbar></my-navbar>\
+                 </div>\
+                 <div class="page-header" style="text-align: center;">\
+                   <h1 id="page-tite"> \
+                     <span  class="all-room-label label label-me"> \
+                       <a href="./all-room.html"> All Room </a>\
+                     </span>\
+                   </h1>\
+                 </div>\
+                 <div class="new-room-box" id="add-room-box">\
+                   <div class="row well well-lg form-group">\
+                     <div class="col-sm-1"><span class="glyphicon glyphicon-plus"></span></div>\
+                     <div class="col-sm-9">\
+                       <input type="text" class="form-control new-room-name" \
+                              placeholder=" room name" v-model="newRoomInput" v-on:keyup.enter="addRoom"> \
+                     </div>\
+                     <div class="col-sm-2">\
+                       <button class="btn btn-success" v-on:click="addRoom">\
+                         <span class="new-btn">Create</span>   \
+                       </button>\
+                     </div>\
+                   </div>\
+                 </div> \
+                 <div class="room-list" id="all-list">\
+                   <div class="row well well-lg" v-for="item in items">\
+                     <div class="col-sm-1"><span class="glyphicon glyphicon-star"></span></div>\
+                     <div class="col-sm-9"><a @click="foo">{{ item.name }}</a></div>\
+                     <div class="col-sm-2">\
+                       <button class="btn btn-warning">\
+                         <span class="join-btn" id="roomid-{{item.roomId}}">Join</span>   \
+                       </button>\
+                     </div>\
+                   </div>\
+                 </div>',
+    data: function () {
+        return allRoomData;
+    },
+    methods: {
+        addRoom: function () {
+            var name = this.newRoomInput.trim()
+            if (name) {
+                allRoomData.items.unshift({ name: name })
+                this.newRoomInput = ''
             }
+        },
+        foo: function () {
+            router.go('/chat-room')
         }
+    }
 })
